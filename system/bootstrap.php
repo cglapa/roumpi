98599150921b8cc8c92ed34da1a00d88a52b861f
Julien Barrier <julien@julienbarrier.fr>
2012-04-30 11:46:16 +0200

    design marche.

--- system/bootstrap.php
+++ system/bootstrap.php
@@ -1,12 +1,12 @@
 <?php defined('IN_CMS') or die('Aucun accès direct n’est autorisé.');
 
 /**
-	Check our environment
+Check our environment
 */
 if(version_compare(PHP_VERSION, '5.3.0', '<')) {
-	// echo and exit with some usful information
-	echo 'Roumpi a besoin d’une version postérieure à PHP 5.3. La votre est PHP ' . PHP_VERSION;
-	exit(1);
+// echo and exit with some usful information
+echo 'Roumpi a besoin d’une version postérieure à PHP 5.3. La votre est PHP ' . PHP_VERSION;
+exit(1);
 }
 
 // get our autoloader
@@ -15,57 +15,57 @@ require PATH . 'system/classes/autoload.php';
 
 // directly map classes for super fast loading
 Autoloader::map(array(
-	'Config' => PATH . 'system/classes/config.php',
-	'Error' => PATH . 'system/classes/error.php',
-	'Session' => PATH . 'system/classes/session.php',
-	'Roumpi' => PATH . 'system/classes/roumpi.php',
-	'Template' => PATH . 'system/classes/template.php',
-	'Request' => PATH . 'system/classes/request.php',
-	'Response' => PATH . 'system/classes/response.php',
-	'Log' => PATH . 'system/classes/log.php',
-	'Db' => PATH . 'system/classes/db.php',
-	'IoC' => PATH . 'system/classes/ioc.php',
-	'Url' => PATH . 'system/classes/url.php'
+'Config' => PATH . 'system/classes/config.php',
+'Error' => PATH . 'system/classes/error.php',
+'Session' => PATH . 'system/classes/session.php',
+'Roumpi' => PATH . 'system/classes/roumpi.php',
+'Template' => PATH . 'system/classes/template.php',
+'Request' => PATH . 'system/classes/request.php',
+'Response' => PATH . 'system/classes/response.php',
+'Log' => PATH . 'system/classes/log.php',
+'Db' => PATH . 'system/classes/db.php',
+'IoC' => PATH . 'system/classes/ioc.php',
+'Url' => PATH . 'system/classes/url.php'
 ));
 
 // tell the autoloader where to find classes
 Autoloader::directory(array(
-	PATH . 'system/classes/'
+PATH . 'system/classes/'
 ));
 
 // register the auto loader
 Autoloader::register();
 
 /**
-	Report all errors let our error class decide which to display
+Report all errors let our error class decide which to display
 */
 error_reporting(-1);
 
 /**
-	Error display will be handled by our error class
+Error display will be handled by our error class
 */
 ini_safe_set('display_errors', false);
 
 /**
-	Disable magic quotes
-	note: magic quotes is deprecated in PHP 5.3
-	src: php.net/manual/en/security.magicquotes.disabling.php
+Disable magic quotes
+note: magic quotes is deprecated in PHP 5.3
+src: php.net/manual/en/security.magicquotes.disabling.php
 */
 if(function_exists('get_magic_quotes_gpc')) {
-	if(get_magic_quotes_gpc()) {
-		ini_safe_set('magic_quotes_gpc', false);
-		ini_safe_set('magic_quotes_runtime', false);
-		ini_safe_set('magic_quotes_sybase', false);
-	}
+if(get_magic_quotes_gpc()) {
+ini_safe_set('magic_quotes_gpc', false);
+ini_safe_set('magic_quotes_runtime', false);
+ini_safe_set('magic_quotes_sybase', false);
+}
 }
 
 /**
-	Check our installation
+Check our installation
 */
 if(Config::load(PATH . 'config.php') === false) {
-	// looks like we are missing a config file
-	echo file_get_contents(PATH . theme_url('/errors/config.php');
-	exit(1);
+// looks like we are missing a config file
+echo file_get_contents(PATH . 'system/admin/theme/error/config.php');
+exit(1);
 }
 
 // Register the default timezone for the application.
@@ -81,21 +81,21 @@ set_error_handler(array('Error', 'native'));
 register_shutdown_function(array('Error', 'shutdown'));
 
 /**
-	Start session handler
+Start session handler
 */
 Session::start();
 
 /**
-	Handle routing
+Handle routing
 */
 Roumpi::run();
 
 /**
-	Close and end session
+Close and end session
 */
 Session::end();
 
 /**
-	Output awesomeness!
+Output awesomeness!
 */
 Response::send();
